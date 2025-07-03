5df0004b839a478d5ae78a43c617ab93fe5bd941
Chase Worthington <chaseworth611@yahoo.com>
2025-07-03 16:50:41 -0500

    update

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+// Add and import your route files here, Example:
+// import yourRoute from './Routers/yourRoute.js'
+
+const app = express();
+
+app.use(express.json());  // To parse JSON bodies in requests
+app.use(cors());          // To allow requests from all origins (good for development)
+
+// Add your route handlers here, Example:
+// app.use('/your-endpoint', yourRoute)
+
+app.get('/', (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(4000, () => {
+  console.log("Listening at http://localhost:4000");
+});
